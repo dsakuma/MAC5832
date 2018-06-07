0d7fc4af6f71f9408511f93c3e5c9a600a156837
Daniel Sakuma <dsakuma@gmail.com>
2018-06-07 17:35:58 -0300

    First commit

---
+++ ep1/notebooks/equacao_normal.py
@@ -0,0 +1,21 @@
+#5619562
+
+import numpy as np
+from numpy.linalg import inv
+
+def normal_equation_prediction(X, y):
+    """
+    Calculates the prediction using the normal equation method.
+    You should add a new column with 1s.
+
+    :param X: design matrix
+    :type X: np.array
+    :param y: regression targets
+    :type y: np.array
+    :return: prediction
+    :rtype: np.array
+    """
+    X = np.append(np.ones((len(X),1), dtype=int), X, axis=1)
+    w = inv(X.T.dot(X)).dot(X.T).dot(y)
+    prediction = np.dot(X, w)
+    return prediction
